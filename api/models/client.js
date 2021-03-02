d45ed926aa468ac564fb9c64eac6620dfd84a596
devian5 <acgarage@outlook.es>
2021-03-02 19:54:54 +0100

    test client

--- api/models/client.js
+++ api/models/client.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    // console.log('ieeee')
+    // console.log('ieeee     aasdasdasd ')
     static associate(models) {
       // define association here
     }
