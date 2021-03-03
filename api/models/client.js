da07b0b54faf745ec42ec556545e83d7e5b3b3c4
Daniel Castillo <danielcastillog92@gmail.com>
2021-03-03 17:03:09 +0100

    DB migrated

--- api/models/client.js
+++ api/models/client.js
@@ -1,26 +1,25 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Client extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    // console.log('ieeee     aasdasdasd ')
-    static associate(models) {
-      // define association here
-    }
-  };
-  Client.init({
-    name: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Client',
-  });
-  return Client;
+    class Client extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    Client.init({
+        name: DataTypes.STRING,
+        phone: DataTypes.STRING,
+        email: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Client',
+    });
+    return Client;
 };
--- api/models/clinic.js
+++ api/models/clinic.js
@@ -1,26 +1,26 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Clinic extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Clinic.init({
-    address: DataTypes.STRING,
-    name: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Clinic',
-  });
-  return Clinic;
+    class Clinic extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    Clinic.init({
+        address: DataTypes.STRING,
+        name: DataTypes.STRING,
+        phone: DataTypes.STRING,
+        email: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Clinic',
+    });
+    return Clinic;
 };
