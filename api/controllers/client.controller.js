59bfe12029f9fbf4cb051f8a2cd99488fde59173
devian5 <acgarage@outlook.es>
2021-03-05 18:40:31 +0100

    I have created of method login in clientController

--- api/controllers/client.controller.js
+++ api/controllers/client.controller.js
@@ -1,23 +1,43 @@
 const { Client } = require('../models');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const client = require('../models/client');
+const secret = process.env.JWT_SECRET || 'danisecret';
+
 
 class ClientController {
 
-   
+    async login(email,password){
+        const user = Client.findOne({email})
+        if(!user){
+            throw new Error('The email does not exist');
+        };
+        if(!await bcrypt.compare(password,client.password)){
+            throw new Error('Wrong password');
+        };
+
+        const payload = {
+            clientId: client.Id,
+            tokenCreationDate: new Date
+        }
+        return jwt.sign(payload, secret)
+    };
+
     async indexAll() {
         return Client.findAll();
-    }
+    };
 
     async create(client){
         return Client.create(client);
-    }
+    };
 
     async findById(id) {
         return Client.findOne({ where: { id } });
-    }
+    };
 
     async findAllByClientId(id) {
         return Client.findAll({ where: { clientId } });
-    }
+    };
 }
 
 let clientController = new ClientController();
